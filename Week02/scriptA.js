3734128f3490b35c2ceaccc689c23ffac70e2a33
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2018-06-12 23:55:21 +0200

    Add files via upload

---
+++ Week02/scriptA.js
@@ -0,0 +1,15 @@
+
+
+for (let i = 1; i<=10 ; i++){
+
+    for (let j = 1 ; j<=i ; j++){
+     
+       
+        document.write("*");  
+       
+    }
+
+      document.write("<br/>");  
+
+    }
+
