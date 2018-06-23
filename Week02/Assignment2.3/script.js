09aab8e9ae0a235298402738430e16c62ba68efd
Zeeshan Haider <39421284+ZeeshanHaider2@users.noreply.github.com>
2018-06-23 21:17:01 +0200

    Update script.js

--- Week02/Assignment2.3/script.js
+++ Week02/Assignment2.3/script.js
@@ -2,25 +2,12 @@
 
 for (let i = 1; i<=10 ; i++){
 
-
-
     for (let j = 1 ; j<=i ; j++){
 
-     
+       document.write("*");  
+ }
 
-       
+       document.write("<br/>");  
 
-        document.write("*");  
-
-       
-
-    }
-
-
-
-      document.write("<br/>");  
-
-
-
-    }
+ }
 
