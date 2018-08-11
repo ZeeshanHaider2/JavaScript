f6f40231665342da9c1ee1ea30b1b3bdf28909e0
Syed Zeeshan Haider <zeeshanhf@gmail.com>
2018-08-11 16:22:22 +0200

    part1

---
+++ Week08/week08.js
@@ -0,0 +1,33 @@
+
+function giveStartAndEnd (start,end){
+    let emptyArrayToBeFilled = [];
+    for (let i=start;i<=end;i++){
+        emptyArrayToBeFilled.push(i)
+    }
+     return emptyArrayToBeFilled;
+}
+
+//const x1= 100;
+//const n = 5 ;
+
+function divisibilityChecker(n){
+    function check(x1){
+    return x1 % n === 0;
+//const  check = x1 =>  x1 % n === 0;
+}
+    return check;
+}
+const arrayOfNumbers = giveStartAndEnd(1,1000);
+console.log(arrayOfNumbers);
+//const divisibleBy03 = divisibilityChecker(2);
+console.log("arrayOfNumbers divided by 3", arrayOfNumbers.filter(divisibilityChecker(3)));
+console.log("arrayOfNumbers divided by 10", arrayOfNumbers.filter(divisibilityChecker(10)));
+console.log("arrayOfNumbers divided by 21", arrayOfNumbers.filter(divisibilityChecker(21)));
+const arrayWithNumbers1To30 = giveStartAndEnd(1,30);
+
+const newArray= arrayWithNumbers1To30.map(x=>{
+    const finalArray = arrayOfNumbers.filter(divisibilityChecker(x));
+    return finalArray.length;
+});
+
+console.log (" Items in array, which are divisible by numbers between 1-30", newArray);
